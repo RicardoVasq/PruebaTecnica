d74e09350b15831f7a39c34d4c200f44d81e23fe
LEGION <rc.vasquez27@gmaill.com>
2025-02-13 10:39:26 -0600

    Agregando el caso 2 a metodologia POM

--- src/test/java/Automation/contactUsTest.java
+++ src/test/java/Automation/contactUsTest.java
@@ -1,5 +1,8 @@
 package Automation;
 import Utilities.BaseTest;
+import Utilities.Logs;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import Page.contactUsPage;
@@ -8,12 +11,30 @@ public class contactUsTest extends BaseTest {
 
 
     @Test
-    public void validarMensajeErrorEnContacto() {
+    public void caso1ErrorMensajeContactaUs() {
         redireccionContactus();
-        //contactUsPage = new ContactUsPage(driver);
+
         contactUsPage.clickSubmitButton();
 
         Assert.assertTrue(contactUsPage.isErrorMessageDisplayed(),
                 "El mensaje de error no se mostró.");
+    }
+
+    @Test
+    public void caso2EnvioFormulario(){
+        redireccionContactus();
+        Logs.info("Seleccionamos la opcion de Webmaster del campo Subject");
+        contactUsPage.opcionContact("1");
+
+        Logs.info("Ingresamos un email de prueba");
+        contactUsPage.ingresarEmail("test12@prueba.com");
+
+        Logs.info("Digiamos el texto en el elemento Message");
+        contactUsPage.ingresarMessage("Linea de texto 1\nLinea de texto 2\nLinea de Texto 3");
+
+        Logs.info("Enviamos la informacion y validamos el envio exitoso");
+        contactUsPage.clickSubmitButton();
+        contactUsPage.mensajeEnvio();
+
     }
 }
--- src/test/java/Page/contactUsPage.java
+++ src/test/java/Page/contactUsPage.java
@@ -5,22 +5,44 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 
 public class contactUsPage extends BaseTest {
 
-    // ✅ Localizadores
+    // Localizadores
     private static By submitButton = By.xpath("//input[@name=\"submitMessage\"]");
     private static By errorMessage = By.xpath("//li[text()='Invalid email address.']");
+    private static By selectContact = By.id("id_contact");
+    private static By campoEmail = By.id("email");
+    private static By campoMessage = By.id("contactform-message");
+    private static By mensajeExitoso = By.xpath("//li[text()='Your message has been successfully sent to our team.']");
 
-    // ✅ Métodos de acción
+    // Métodos de acción
     public static void clickSubmitButton() {
         driver.findElement(submitButton).click();
     }
 
+    //Identificamos elementos de la pagina
     public static boolean isErrorMessageDisplayed() {
         WebElement errorElement = driver.findElement(errorMessage);
         return errorElement.isDisplayed();
     }
+
+    public static void opcionContact(String opcion){
+        WebElement select = driver.findElement(selectContact);
+        Select dropdwon = new Select(select);
+        dropdwon.selectByValue(opcion);
+    }
+
+    public static void ingresarEmail(String email){
+        driver.findElement(campoEmail).sendKeys(email);
+    }
+
+    public static void ingresarMessage(String message){
+        driver.findElement(campoMessage).sendKeys(message);
+    }
+    public static boolean mensajeEnvio() {
+        return driver.findElement(mensajeExitoso).isDisplayed();}
 
 }
